c1304e0ec1e9687d3f7caf57376406dffedd0edb
Stanley Cooper <scooperlu@gmail.com>
2022-05-23 10:38:27 -0400

    original data from prior

---
+++ server/src/data/runs-data.js
@@ -0,0 +1,145 @@
+module.exports = [
+    {
+        id: 1,
+        date: new Date("2022-03-18"),
+        time: "30:09",
+        distance: 2.42,
+        avgPace: "12:26",
+        bestPace: "8:19",
+        avgHeartRate: 176,
+        calories: 353,
+        rating: "N/A"
+    },
+    {
+        id: 2,
+        date: new Date("2022-03-21"),
+        time: "24:39",
+        distance: 2.30,
+        avgPace: "10:43",
+        bestPace: "7:51",
+        avgHeartRate: 158,
+        calories: 310,
+        rating: "N/A"
+    },
+    {
+        id: 3,
+        date: new Date("2022-03-23"),
+        time: "33:22",
+        distance: 3.20,
+        avgPace: "10:26",
+        bestPace: "8:24",
+        avgHeartRate: 165,
+        calories: 449,
+        rating: "N/A"
+    },
+    {
+        id: 4,
+        date: new Date("2022-03-24"),
+        time: "19:11",
+        distance: 2.01,
+        avgPace: "9:32",
+        bestPace: "8:07",
+        avgHeartRate: 168,
+        calories: 272,
+        rating: "6/10"
+    },
+    {
+        id: 5,
+        date: new Date("2022-03-28"),
+        time: "36:52",
+        distance: 3.15,
+        avgPace: "11:42",
+        bestPace: "9:39",
+        avgHeartRate: 156,
+        calories: 433,
+        rating: "9/10"
+    },
+    {
+        id: 6,
+        date: new Date("2022-03-30"),
+        time: "36:27",
+        distance: 3.23,
+        avgPace: "11:17",
+        bestPace: "9:35",
+        avgHeartRate: 158,
+        calories: 447,
+        rating: "6/10"
+    },
+    {
+        id: 7,
+        date: new Date("2022-04-01"),
+        time: "31:13",
+        distance: 3.13,
+        avgPace: "9:59",
+        bestPace: "8:29",
+        avgHeartRate: 162,
+        calories: 415,
+        rating: "N/A"
+    },
+    {
+        id: 8,
+        date: new Date("2022-04-02"),
+        time: "31:16",
+        distance: 3.09,
+        avgPace: "10:08",
+        bestPace: "7:49",
+        avgHeartRate: 162,
+        calories: 413,
+        rating: "N/A"
+    },
+    {
+        id: 9,
+        date: new Date("2022-04-04"),
+        time: "29:32",
+        distance: 3.09,
+        avgPace: "9:34",
+        bestPace: "7:40",
+        avgHeartRate: 169,
+        calories: 412,
+        rating: "N/A"
+    },
+    {
+        id: 10,
+        date: new Date("2022-04-06"),
+        time: "39:03",
+        distance: 4.01,
+        avgPace: "9:45",
+        bestPace: "8:30",
+        avgHeartRate: 178,
+        calories: 567,
+        rating: "N/A"
+    },
+    {
+        id: 11,
+        date: new Date("2022-04-08"),
+        time: "32:26",
+        distance: 3.12,
+        avgPace: "10.24",
+        bestPace: "8:44",
+        avgHeartRate: 161,
+        calories: 411,
+        rating: "N/A"
+    },
+    {
+        id: 12,
+        date: new Date("2022-04-09"),
+        time: "27:36",
+        distance: 2.60,
+        avgPace: "10:37",
+        bestPace: "9:11",
+        avgHeartRate: 156,
+        calories: 340,
+        rating: "N/A"
+    },
+    {
+        id: 13,
+        date: new Date("2022-04-11"),
+        time: "32:58",
+        distance: 3.03,
+        avgPace: "10.53",
+        bestPace: "9:29",
+        avgHeartRate: 156,
+        calories: 397,
+        rating: "N/A"
+    },
+]
