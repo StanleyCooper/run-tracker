d9c9948bfe3f1bce2b39667f11226178e9de0d57
Stanley Cooper <scooperlu@gmail.com>
2022-05-23 10:29:43 -0400

    run log components implemented

---
+++ client/src/components/RunEntry.js
@@ -0,0 +1,34 @@
+
+import React from "react";
+
+function RunEntry({runs}) {
+    const runEntries = runs.map((run) =>
+        <tr key={run.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                {new Date(run.date).toLocaleDateString()}
+            </th>
+            <td className="px-6 py-4">
+                {run.distance}
+            </td>
+            <td className="px-6 py-4">
+                {run.avgPace}
+            </td>
+            <td className="px-6 py-4">
+                {run.bestPace}
+            </td>
+            <td className="px-6 py-4">
+                {run.avgHeartRate}
+            </td>
+            <td className="px-6 py-4">
+                {run.calories}
+            </td>
+            <td className="px-6 py-4">
+                {run.rating}
+            </td>
+        </tr>
+    );
+
+   return (<tbody>{runEntries}</tbody>);
+}
+
+export default RunEntry;
---
+++ client/src/components/RunEntryList.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import RunEntry from "./RunEntry";
+
+function RunEntryList() {
+
+    const [runs, setRuns] = useState([]);
+
+    useEffect(() => {
+        console.log("useEffect");
+        async function loadRuns() {
+            const response = await fetch(
+                'http://localhost:5000/runs'
+            );
+            const runsFromAPI = await response.json();
+            console.log("setRuns", runsFromAPI);
+            setRuns(runsFromAPI.data);
+        }
+        loadRuns();
+    }, []);
+
+    return (
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Date
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Distance
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Avg. Pace
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Best Pace
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Avg Heart Rate
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Calories
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Rating
+                        </th>
+                    </tr>
+                </thead>
+                <RunEntry runs={runs}/>
+            </table>
+        </div>
+    )
+}
+
+export default RunEntryList;
